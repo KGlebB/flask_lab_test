e06b057a773bcb04da86936cee85b93b42bd9984
Gleb nikiforov <nikiforov02@list.ru>
2025-01-18 16:49:34 +0300

    ADD Flask-Migrate

--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@ pip install Flask
 pip install scikit-learn nltk
 pip install Flask-SQLAlchemy Flask-Login
 pip install Bootstrap-Flask
+pip install Flask-Migrate
 ```
 * Не путать Flask-Bootstrap (v.3) и Bootstrap-Flask (v.4 и v.5)
 
--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 from app import create_app, db
+from flask_migrate import Migrate, upgrade, migrate, init
+import os
 
 app = create_app()
+Migrate(app, db)
 
 if __name__ == '__main__':
   with app.app_context():
-    db.create_all()
+    if not os.path.exists('migrations'):
+      init()
+    migrate()
+    upgrade()
+
   app.run(debug=True)
